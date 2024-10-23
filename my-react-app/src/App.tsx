c709c9b1b2435db0e7d242fe2144659d0869dfba
Ludovic Pouillie <ludovicpouillie@gmail.com>
2024-10-23 15:50:33 +0200

    ADD quête6

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,31 +1,92 @@
 import "./App.css";
 import PokemonCards from "./components/PokemonCard";
+import { useState } from "react";
+
+
+
 
 
 const pokemonList = [
-  {  
-    name: "bulbasaur",  
-    imgSrc: 
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",  
-  },  
-  {  
-    name: "mew",  
-  },  
-];
+
+  {
+  
+      name: "bulbasaur",
+  
+      imgSrc:
+  
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  
+    },
+  
+    {
+  
+      name: "charmander",
+  
+      imgSrc:
+  
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  
+    },
+  
+    {
+  
+      name: "squirtle",
+  
+      imgSrc:
+  
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  
+    },
+  
+    {
+  
+      name: "pikachu",
+  
+      imgSrc:
+  
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  
+    },
+  
+    {
+  
+      name: "mew",
+  
+    },
+  
+  ];
+
+
 
 function App() {
+    const [pokemonIndex, setPokemonIndex] = useState(0);
 
+    const handleBackClick = () => {
+        setPokemonIndex(pokemonIndex - 1)
+    }
+    
+    const handleNextClick = () => {
+      setPokemonIndex(pokemonIndex + 1)
+    }
   return (
 
     <div>
 
-      <PokemonCards pokemon={pokemonList[0]} />
+      <PokemonCards pokemon={pokemonList[pokemonIndex]} />
+
+        {pokemonIndex > 0 &&
+
+          <button type="button" onClick={handleBackClick}>Précédent</button>}
+
+        {pokemonIndex < pokemonList.length - 1 &&
+
+          <button type="button" onClick={handleNextClick}>Suivant</button>}
 
     </div>
 
-  );
+);
 
 }
 
 
-export default App;
+export default App
